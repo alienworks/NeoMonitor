efa6b85d3f6b42c6df58727ffe886d57dcbf2f5e
amagiddmxh <1107537248@qq.com>
2020-04-23 19:40:27 +0800

    Add Analysis page, hidden router

---
+++ src/components/Analysis.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container class="an-container text-left">
+    <h1 class="mt-5">Analysis</h1>
+    
+  </b-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.an-container {
+  background-color: rgba(255, 255, 255, 0);
+
+  @media (min-width: 768px) {
+    padding-left: 15px;
+  }
+
+  @media (min-width: 992px) {
+    padding-left: 7rem;
+  }
+
+  @media (min-width: 1200px) {
+    padding-left: 10rem;
+  }
+}  
+</style>
