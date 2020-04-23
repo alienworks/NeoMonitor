0ff1f15cbf65275e9ad6db290a8e0de2c0a488d0
amagiddmxh <1107537248@qq.com>
2020-04-23 18:02:50 +0800

    Update service base urls

--- src/services/NodeService.js
+++ src/services/NodeService.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 import { combine } from "@/utils";
 
-const baseNodesUrl = process.env.VUE_APP_RESTAPI;
-const rawMempoolUrl = combine(baseNodesUrl, 'rawmempool');
+const baseUrl = process.env.VUE_APP_RESTAPI;
+
+const nodesUrl = combine(baseUrl, 'nodes');
+const rawMempoolUrl = combine(nodesUrl, 'rawmempool');
+
+const matrixUrl = combine(baseUrl, 'matrix');
+
 
 export default {
   getNodeInfo(nodeID) {
-    return axios.get(combine(baseNodesUrl, nodeID));
+    return axios.get(combine(nodesUrl, nodeID));
   },
   getNodesInfo() {
-    return axios.get(baseNodesUrl);
+    return axios.get(nodesUrl);
   },
   getRawMempool(nodeID) {
     return axios.get(combine(rawMempoolUrl, nodeID));
+  },
+  getMatrixItems() {
+    return axios.get(matrixUrl)
   }
 };
