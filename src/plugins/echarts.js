226841b931389183dba5ee597baf79265810e609
ElbertYe <298217565@qq.com>
2020-06-07 17:08:44 +0800

    import echarts components and libs

--- src/plugins/echarts.js
+++ src/plugins/echarts.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import Echarts from "vue-echarts";
+import "echarts/lib/chart/heatmap";
 import "echarts/lib/chart/bar";
 import "echarts/lib/chart/scatter";
+import "echarts/lib/component/visualMap";
+import "echarts/lib/component/calendar";
 Vue.component("chart", Echarts);
