a6fee2cd05f534b9ad447c1654228c323e99ede1
ElbertYe <298217565@qq.com>
2020-06-21 11:46:03 +0800

    frontend pagination for exception list

--- src/components/NodeInfo.vue
+++ src/components/NodeInfo.vue
@@ -10,24 +10,28 @@
 
     <a-spin :spinning="isFetchingProgress">
       <a-table
+        v-if="myNodeInfo"
         :columns="fields"
-        :data-source="nodeInfo"
+        :data-source="myNodeInfo"
         :pagination="false"
         :scroll="{ x: 'max-content', y: 'max-content' }"
         size="small"
       >
-        <a
-          :href="`//neoscan.io/block/${h}`"
-          target="_blank"
-          slot="height"
-          slot-scope="h"
-          >{{ h }}</a
-        >
+        <a :href="`//neoscan.io/block/${h}`" target="_blank" slot="height" slot-scope="h">{{ h }}</a>
 
-        <router-link slot="time" slot-scope="t" to="/statistics">{{
+        <router-link slot="time" slot-scope="t" to="/statistics">
+          {{
           t
-        }}</router-link>
+          }}
+        </router-link>
       </a-table>
+      <a-pagination
+        v-if="nodeInfo && nodeInfo.length>0"
+        @change="changePage"
+        v-model="current"
+        :total="nodeInfo.length"
+        pagesize.sync="10"
+      />
     </a-spin>
   </div>
 </template>
@@ -45,45 +49,61 @@ export default {
           key: "id",
           dataIndex: "id",
           title: "ID",
-          sorter: sorter("id"),
+          sorter: sorter("id")
         },
         {
           key: "nodeName",
           dataIndex: "nodeName",
           title: "Nodes",
-          sorter: sorter("nodeName"),
+          sorter: sorter("nodeName")
         },
         {
           key: "exceptionHeight",
           dataIndex: "exceptionHeight",
           title: "Height",
           sorter: sorter("exceptionHeight"),
-          scopedSlots: { customRender: "height" },
+          scopedSlots: { customRender: "height" }
         },
         {
           key: "exceptionTime",
           dataIndex: "exceptionTime",
           title: "Generate Time(GMT +8)",
           sorter: sorter("exceptionTime"),
-          scopedSlots: { customRender: "time" },
+          scopedSlots: { customRender: "time" }
         },
         {
           key: "intervals",
           dataIndex: "intervals",
           title: "Interval(s)",
-          sorter: sorter("intervals"),
-        },
+          sorter: sorter("intervals")
+        }
       ],
       filter: null,
-      filterBouncer: debounce((val) => this.filterTable(val)),
+      filterBouncer: debounce(val => this.filterTable(val)),
+      current: 1,
+      myNodeInfo: []
     };
   },
   mounted() {
     this.$store.dispatch("getNodeInfo");
   },
   computed: {
-    ...mapGetters(["nodeInfo", "isFetchingProgress"]),
+    ...mapGetters(["nodeInfo", "isFetchingProgress"])
   },
+  methods: {
+    changePage(page, size) {
+      this.myNodeInfo = this.nodeInfo.slice((page - 1) * size, page * size);
+    }
+  },
+  watch: {
+    nodeInfo(val) {
+      if (val && val.length >= 10) {
+        this.myNodeInfo = val.slice(0, 10);
+      } else {
+        this.myNodeInfo = val.slice(0, val.length);
+      }
+    }
+  }
 };
 </script>
 
