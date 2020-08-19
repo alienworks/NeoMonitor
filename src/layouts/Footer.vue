7d059cfa180da542692ef1644b3df8055945f079
Fortunesky <fortuneskylin@hotmail.com>
2020-08-19 11:23:15 +0800

    clean main.js & hide some icons in Footer temporarily;

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -11,34 +11,33 @@
         <div class="copyright">©️Copyright Alienworks 2017-2020</div>
       </a-col>
       <a-col :span="7" class="clearfix">
-        <div class="icons website">
+        <!-- <div class="icons website">
           <img width="24px" height="24px" src="../assets/icons/icon-website.png" />
-        </div>
-        <div class="icons twitter">
+        </div> -->
+        <!-- <div class="icons twitter">
           <img width="24px" height="24px" src="../assets/icons/icon-twitter.png" />
-        </div>
+        </div> -->
         <div class="icons github">
           <a href="http://github.com/alienworks"><img width="24px" height="24px" src="../assets/icons/icon-github.png" /></a>
         </div>
-        <div class="icons slack">
+        <!-- <div class="icons slack">
           <img width="24px" height="24px" src="../assets/icons/icon-slack.png" />
-        </div>
-        <div class="icons reddit">
+        </div> -->
+        <!-- <div class="icons reddit">
           <img width="24px" height="24px" src="../assets/icons/icon-reddit.png" />
-        </div>
-        <div class="icons discord">
+        </div> -->
+        <!-- <div class="icons discord">
           <img width="24px" height="24px" src="../assets/icons/icon-discord.png" />
-        </div>
-        <div class="icons wechat">
+        </div> -->
+        <!-- <div class="icons wechat">
           <img width="24px" height="24px" src="../assets/icons/icon-wechat.png" />
-        </div>
-        <div class="icons weibo">
+        </div> -->
+        <!-- <div class="icons weibo">
           <img width="24px" height="24px" src="../assets/icons/icon-weibo.png" />
-        </div>
-        <div class="icons youtube">
+        </div> -->
+        <!-- <div class="icons youtube">
           <img width="24px" height="24px" src="../assets/icons/icon-youtube.png" />
-        </div>
-
+        </div> -->
       </a-col>
     </a-row>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import "@babel/polyfill";
-import "mutationobserver-shim";
-import Vue from "vue";
-import App from "./App";
-import Vuex from "vuex";
-import { store } from "./store";
-import router from "./router";
-import BootstrapVue from "bootstrap-vue";
-import "@/plugins/echarts";
+import "@babel/polyfill"
+import "mutationobserver-shim"
+import Vue from "vue"
+import App from "./App"
+import Vuex from "vuex"
+import store from "./store"
+import router from "./router"
+import BootstrapVue from "bootstrap-vue"
+import "@/plugins/echarts"
 
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
+import Antd from "ant-design-vue"
+import "ant-design-vue/dist/antd.css"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(Vuex);
-Vue.use(BootstrapVue);
-Vue.use(Antd);
+Vue.use(Vuex)
+Vue.use(BootstrapVue)
+Vue.use(Antd)
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount("#app")
